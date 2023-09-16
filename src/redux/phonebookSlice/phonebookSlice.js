c3eef2a3a799532e30bce5619170b10210ed3122
Boris <91095836+BorisKorsun@users.noreply.github.com>
2023-09-16 16:56:42 +0200

    fixes redux persist non-serializable value

--- src/redux/phonebookSlice/phonebookSlice.js
+++ src/redux/phonebookSlice/phonebookSlice.js
@@ -2,23 +2,45 @@ import { createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+import { nanoid } from 'nanoid';
+
 const persistConfig = {
   key: 'phonebook',
   storage,
 };
 
-const initialState = {a: 1};
+const initialState = {
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+};
 
 const phonebookSlice = createSlice({
   name: 'phonebook',
   initialState,
   reducers: {
-    add: (state, action) => {
-      state.push(action.payload);
+    add: {
+      reducer(state, action) {
+        return state.push(action.payload);
+      },
+      prepare(name) {
+        return {
+          payload: {
+            name,
+            id: nanoid(),
+          },
+        };
+      },
     },
   },
 });
 
-export const persistedReducer = persistReducer(persistConfig, phonebookSlice.reducer);
+export const persistedReducer = persistReducer(
+  persistConfig,
+  phonebookSlice.reducer
+);
 
 export const { add } = phonebookSlice.actions;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { persistStore } from 'redux-persist';
 import { persistedReducer } from 'redux/phonebookSlice/phonebookSlice';
 
@@ -6,6 +14,12 @@ export const store = configureStore({
   reducer: {
     phonebook: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
